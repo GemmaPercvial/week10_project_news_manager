012604c31e98a5337d017d2e38acf1deecf4ad98
Gemma <gemma.percival@hotmail.co.uk>
2018-10-01 17:02:42 +0100

    add to heroku

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -14,10 +14,12 @@ import java.util.Map;
 
 import static spark.Spark.get;
 import static spark.Spark.post;
+import static spark.SparkBase.port;
 import static spark.SparkBase.staticFileLocation;
 
 public class MainController {
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
 
         Seeds.seedData();
         staticFileLocation("/public");
